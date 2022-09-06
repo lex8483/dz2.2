2169fb6936cc2875b2d8b3f0b87d6257ae2c69ed
lex8483 <lex8483@mail.ru>
2022-09-06 11:27:35 +0300

    dz2.2.1-1

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,2 +1,8 @@
-public class Bicycle {
+public class Bicycle extends ServiceStation {
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
 }
--- src/Truck.java
+++ src/Truck.java
@@ -1,2 +1,16 @@
-public class Truck {
+public class Truck extends ServiceStation {
+    public Truck(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
 }
